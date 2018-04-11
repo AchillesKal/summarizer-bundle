eb323c1c7aa316b58486a45191a1943f809b1438
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-04-11 23:04:22 +0300

    adds controller to the bundle

--- src/Controller/TextSummarizerApiController.php
+++ src/Controller/TextSummarizerApiController.php
@@ -8,8 +8,25 @@
 
 namespace AchillesKal\SummarizerBundle\Controller;
 
+use AchillesKal\SummarizerBundle\TextSummarizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class TextSummarizerApiController
+
+class TextSummarizerApiController extends AbstractController
 {
+    private $textSummarizer;
 
+    public function __construct(TextSummarizer $textSummarizer)
+    {
+        $this->textSummarizer = $textSummarizer;
+    }
+
+    public function index()
+    {
+        $text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin iaculis quis diam sit amet lacinia. Cras iaculis in sapien vitae ullamcorper. Quisque magna libero, blandit sit amet maximus eu, dictum non dolor. Duis vulputate non enim ac condimentum. Cras viverra quam id magna tincidunt, vitae commodo enim fermentum. Proin et massa a nulla suscipit ullamcorper vel ut tortor. Curabitur ex libero, elementum dapibus eros faucibus, varius sodales nisl. Aliquam rutrum fringilla augue in congue. Nam interdum fermentum est, ac semper mauris finibus vitae.";
+
+        return $this->json([
+            'summary' => $this->textSummarizer->summarize($text)
+        ]);
+    }
 }

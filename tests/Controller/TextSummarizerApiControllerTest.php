ac3c5ed1c49950df93444e5f8f702e7498fad3f5
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-04-12 01:31:09 +0300

    finishes functional test

--- tests/Controller/TextSummarizerApiControllerTest.php
+++ tests/Controller/TextSummarizerApiControllerTest.php
@@ -11,21 +11,31 @@ namespace AchillesKal\SummarizerBundle\Tests\Controller;
 
 use AchillesKal\SummarizerBundle\AchillesKalSummarizerBundle;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
 
 class TextSummarizerApiControllerTest extends TestCase
 {
     public function testIndex()
     {
+        $kernel = new AchillesKalSummarizerControllerKernel();
+        $client = new Client($kernel);
+        $client->request('GET', '/api/');
 
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
     }
 }
 
 
 class AchillesKalSummarizerControllerKernel extends Kernel
 {
+    use MicroKernelTrait;
+
     public function __construct()
     {
         parent::__construct('test', true);
@@ -35,14 +45,22 @@ class AchillesKalSummarizerControllerKernel extends Kernel
     {
         return [
             new AchillesKalSummarizerBundle(),
+            new FrameworkBundle()
         ];
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
+    protected function configureRoutes(RouteCollectionBuilder $routes)
     {
-        $loader->load(function(ContainerBuilder $container) {
-        });
+        $routes->import(__DIR__.'/../../src/Resources/config/routes.xml', '/api');
     }
+
+    protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
+    {
+        $c->loadFromExtension('framework', [
+            'secret' => 'F00',
+        ]);
+    }
+
 
     public function getCacheDir()
     {

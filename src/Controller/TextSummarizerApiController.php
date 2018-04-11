81ed6e0db04fc825dc8b0bbde8d800e01be18c25
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-04-11 22:26:40 +0300

    adds api controller

---
+++ src/Controller/TextSummarizerApiController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: achilleskal
+ * Date: 4/11/18
+ * Time: 10:26 PM
+ */
+
+namespace AchillesKal\SummarizerBundle\Controller;
+
+
+class TextSummarizerApiController
+{
+
+}
